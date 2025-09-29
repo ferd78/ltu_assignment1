dfe614633a7039f6c271893677a09087ba428d9b
ferd78 <felix09ferdinand@gmail.com>
2025-09-30 01:15:23 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,19 @@ import StepButton from "./StepButton";
 
 export default function Home() {
   return (
-    <div className="w-screen flex justify-evenly pt-8 gap-32 font-semibold text-white">
-      <div className="flex flex-col gap-4">
-        <h1> Tabs </h1>
+    <div className="w-screen flex justify-around pt-12 gap-72 font-semibold text-white pr-12">
+
+      <div className="flex flex-col">
+        <div className="absolute left-10 top-48 ">
+          <h1> Tabs </h1>
+        </div>
         <h1> Tabs Headers:[+]</h1>
-        <StepButton number={1}/>
+        <div className="flex flex-col gap-4 pt-5">
+          <StepButton number={1}/>
+          <StepButton number={2}/>
+          <StepButton number={3}/>
+        </div>
+       
       </div>
 
       <h1> Tabs Content</h1>
